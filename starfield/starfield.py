58991deeac46ca8c51d23761266ce136aee8fce0
Tobias Balle-Petersen <tobiasbp@gmail.com>
2017-05-11 22:59:29 +0200

    Added player

--- starfield/starfield.py
+++ starfield/starfield.py
@@ -10,6 +10,9 @@ http://programarcadegames.com/python_examples/show_file.php?file=moving_sprites.
 # Desired framerate in frames per second. Try out other values.              
 FPS = 30
 
+PLAYER_ROT_SPEED = 6
+PLAYER_ACCELERATION = 1
+
 #the next line is only needed for python2.x and not necessary for python3.x
 #from __future__ import print_function, division
 import pygame
@@ -42,12 +45,11 @@ class Star(pygame.sprite.Sprite):
         """ Constructor. Pass in the color of the block,
         and its x and y position. """
         
-        # Set layer
-        self._layer = size
-        
         # Call the parent class (Sprite) constructor
         super().__init__()
  
+        # Set layer
+        self._layer = size
         
         # Create an empty image (surface). Needed by PyGame.
         # This could also be an image loaded from the disk.
@@ -82,8 +84,9 @@ class Star(pygame.sprite.Sprite):
         """ Move star """
         
         # Update position (float)
-        self.pos_x += player['dx'] * self.speed
-        self.pos_y += player['dy'] * self.speed
+        # Subtract as stars move opposite to player
+        self.pos_x -= player['dx'] * self.speed
+        self.pos_y -= player['dy'] * self.speed
         
         # Move to the updated position (integer)
         self.rect.x = self.pos_x
@@ -100,6 +103,56 @@ class Star(pygame.sprite.Sprite):
             #self.pos_y =- screen_height
             self.pos_y = 0
  
+
+class Player(pygame.sprite.Sprite):
+    """
+    This class represents a player
+    It derives from the "Sprite" class in Pygame
+    """
+    def __init__(self, size=40):
+        """ Constructor. Pass in the color of the block,
+        and its x and y position. """
+        
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+ 
+        # Set layer
+        self._layer = 100
+        
+        # Create an empty image (surface). Needed by PyGame.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.Surface([size, size])
+        
+        # Unlikely color is transparent background
+        self.image.fill([1,2,3])
+        self.image.set_colorkey([1,2,3])
+        
+        # Draw space ship
+        pygame.draw.polygon(self.image, WHITE, ((size,size/2),(0,3*size/4),(0,size/4))) 
+
+        # Save original image as rotation is destructive
+        self.image_orig = self.image
+                
+        # Fetch the rectangle object that has the dimensions of the image
+        # image.
+        # Update the position of this object by setting the values
+        # of rect.x and rect.y
+        # Needed by PyGame
+        self.rect = self.image.get_rect()
+        
+        self.rect.center = (screen_width/2, screen_height/2)
+        
+    def update(self):
+        """ Move star """
+        # Rotated copies of image and rect
+        self.rot_image = pygame.transform.rotate(self.image_orig, -player['dir'])
+        self.rot_rect = self.rot_image.get_rect(center=self.rect.center)
+        # Update sprite to new rotated image & rect
+        self.image = self.rot_image
+        self.rect = self.rot_rect
+        
+        
+
  
 # This is a list of 'sprites.' Each star in the program is
 # added to this list. The list is managed by a class called 'Group.'
@@ -118,8 +171,8 @@ for i in range(50):
     # Add the star to the list of stars
     star_group.add(star)
     
-
-
+player_group = pygame.sprite.LayeredUpdates()
+player_group.add(Player())
 
 # Create Pygame clock object.  
 clock = pygame.time.Clock()
@@ -139,6 +192,7 @@ while mainloop:
     
     # Process events
     for event in pygame.event.get():
+    
         # User presses QUIT-button.
         if event.type == pygame.QUIT:
             mainloop = False 
@@ -146,31 +200,37 @@ while mainloop:
             # User presses ESCAPE-Key
             if event.key == pygame.K_ESCAPE:
                 mainloop = False
-            if event.key == pygame.K_UP:
-                player['speed'] += 1    
-            if event.key == pygame.K_DOWN:
-                player['speed'] -= 1
-            if event.key == pygame.K_LEFT:
-                player['dir'] += 10
-            if event.key == pygame.K_RIGHT:
-                player['dir'] -= 10
-            
+                            
+    # Get pressed/held status of all keys        
+    pressed = pygame.key.get_pressed()
+    
+    if pressed[pygame.K_LEFT]:
+        player['dir'] -= PLAYER_ROT_SPEED
+    if pressed[pygame.K_RIGHT]:
+        player['dir'] += PLAYER_ROT_SPEED
+    if pressed[pygame.K_UP]:
+        player['speed'] += PLAYER_ACCELERATION
+    if pressed[pygame.K_DOWN]:
+        player['speed'] -= PLAYER_ACCELERATION
+        
     # Print framerate and playtime in titlebar.
     text = "FPS: {0:.2f}   Playtime: {1:.2f}".format(clock.get_fps(), playtime)
     pygame.display.set_caption(text)
     
     # How far did player move
-    player['dx'] = sin(radians(player['dir'])) * player['speed']
-    player['dy'] = cos(radians(player['dir'])) * player['speed']
+    player['dx'] = cos(radians(player['dir'])) * player['speed']
+    player['dy'] = sin(radians(player['dir'])) * player['speed']
     
     # Clear screen (paint)
     screen.fill(BLACK)
     
-    # Update all stars
+    # Update sprites
     star_group.update()
+    player_group.update()
     
-    # Draw all stars on the surface screen 
+    # Draw all sprites on the surface screen 
     star_group.draw(screen)
+    player_group.draw(screen)
     
    
     
